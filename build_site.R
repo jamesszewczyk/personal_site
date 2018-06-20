dcb43edb7a892a9e6e147a56a54218e22ee39390
jamesszewczyk <james.szewczyk@emory.edu>
2018-06-20 08:56:00 -0400

    Website commit

--- build_site.R
+++ build_site.R
@@ -6,7 +6,6 @@ rmarkdown::render_site()
 
 
 ## Use this code to push to github in shell:
-
 #Let's add all the files to our git staging area
 git add -A #the -A flag tells it git you want everything
 
